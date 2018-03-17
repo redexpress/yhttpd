274fbf333658189b4a594d9f8a67ed33eaf4ec8d
Gavin Yang <redppl@hotmail.com>
2018-03-17 17:52:55 +0800

    add base64

--- ylib.h
+++ ylib.h
@@ -2,6 +2,9 @@
 #define ylib_h
 
 #include <sys/types.h>
+
 int substring(char *dst, const char *src, size_t begin, size_t end);
+int base64encode(char *dst, const unsigned char *src, size_t len, size_t *out_len);
+int base64decode(unsigned char *dst, const char *src, size_t len, size_t *out_len);
 
 #endif /* ylib_h */
