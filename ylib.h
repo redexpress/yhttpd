124929906a3263262a35c0e80ff7149b32d9ccf5
Gavin Yang <redppl@hotmail.com>
2018-03-17 21:42:02 +0800

    Update ylib.h

--- ylib.h
+++ ylib.h
@@ -6,5 +6,6 @@
 int substring(char *dst, const char *src, size_t begin, size_t end);
 int base64encode(char *dst, const unsigned char *src, size_t len, size_t *out_len);
 int base64decode(unsigned char *dst, const char *src, size_t len, size_t *out_len);
+void md5(const uint8_t *initial_msg, size_t initial_len, uint8_t *digest);
 
 #endif /* ylib_h */
