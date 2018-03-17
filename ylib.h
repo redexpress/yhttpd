9c7a8c6735887c73b7a7a9135b083ce63eed76aa
Gavin Yang <redppl@hotmail.com>
2018-03-17 22:05:22 +0800

    Update ylib.h

--- ylib.h
+++ ylib.h
@@ -2,6 +2,7 @@
 #define ylib_h
 
 #include <sys/types.h>
+#include <stdint.h>
 
 int substring(char *dst, const char *src, size_t begin, size_t end);
 int base64encode(char *dst, const unsigned char *src, size_t len, size_t *out_len);
